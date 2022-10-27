e8cebab7263ba307e2bf96023cec5ef27a3d73f9
Nerbles1 <94391712+Nerbles1@users.noreply.github.com>
2022-10-27 18:41:10 -0500

    porting website from school computer :skull:

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-// this is useless lol i probs wont ever use this file but its here ig
+function openNav() {
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+   }
+ /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+// NavBar pasted from W3Schools :skull: but the one on home.html is original code 

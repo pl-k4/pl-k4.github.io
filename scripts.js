7f0e238194cccf2dab457e53e8978df3c14b69d2
Nerbles1 <94391712+Nerbles1@users.noreply.github.com>
2023-11-21 23:28:52 -0600

    fix image and add javascript animation

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 function soon() {
     alert("Coming soon!");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Fade in the header with a delay
+    var header = document.querySelector('.header');
+    var sectionTitle = document.querySelector('.section-title');
+    setTimeout(function() {
+        header.style.opacity = 1;
+        sectionTitle.style.opacity = 1;
+    }, 100); 
+
+    var cards = document.querySelectorAll('.card');
+    cards.forEach(function(card, index) {
+        setTimeout(function() {
+            card.style.opacity = 1;
+        }, 150 * index);
+    });
+});
